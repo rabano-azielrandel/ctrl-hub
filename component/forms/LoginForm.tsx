25d3bb3b16df5f732894f4df86a7c1123cd65196
Aziel Randel Rabano <imazielr@gmail.com>
2026-04-28 09:06:10 +0800

    initial loginform

---
+++ component/forms/LoginForm.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useState } from "react";
+
+export default function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const res = await fetch("/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+    } catch (err: any) {
+      setError(err.message || "Something went wrong");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <form onSubmit={() => console.log("hello")} className="border">
+      <div className="">sdfadsa</div>
+    </form>
+  );
+}
