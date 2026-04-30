871ff5fa201ad135ce0c97c0edc5eddc99e372bc
Aziel Randel Rabano <imazielr@gmail.com>
2026-04-30 21:44:30 +0800

    update page fetch data initially

--- app/(protected)/projects/page.tsx
+++ app/(protected)/projects/page.tsx
@@ -1,10 +1,15 @@
 import Main from "@/component/sections/projects/main";
 import { ProjectPanel } from "@/component/sections/projects/projects";
-import { getProjects, getProjectsRows } from "@/app/actions/projectActions";
+import {
+  getProjects,
+  getProjectsRows,
+  getProjectsCardRows,
+} from "@/app/actions/projectActions";
 
 const projectPanelData = await getProjects();
 const projectRows = await getProjectsRows();
-console.log("projects: ", projectRows);
+const projectCardRows = await getProjectsCardRows();
+console.log("projects: ", projectCardRows);
 
 export default function Projects() {
   return (
