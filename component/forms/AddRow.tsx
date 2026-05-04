3d373ee3479b43225fd8e427317a895a693338db
Aziel Randel Rabano <imazielr@gmail.com>
2026-05-04 22:34:01 +0800

    update ui in addRow form

--- component/forms/AddRow.tsx
+++ component/forms/AddRow.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useState } from "react";
+import Button from "../ui/Button";
 import Input from "../ui/Input";
 
 type colsObj = {
@@ -13,19 +15,33 @@ interface Props {
 }
 
 export default function AddRow({ cols, tableName }: Props) {
+  const [values, setValues] = useState<Record<string, string>>({});
+
+  const handleChange = (key: string, value: string) => {
+    setValues((prev) => ({
+      ...prev,
+      [key]: value,
+    }));
+  };
+
   return (
-    <div className="w-40 flex flex-col">
+    <form className="w-full max-w-2xl p-4">
       <h2>{tableName}</h2>
-      {cols.map((item, index) => (
-        <div key={index} className="flex">
-          <Input
-            label={item.label}
-            type="text"
-            value={""}
-            placeholder={item.key}
-          />
+
+      <div className="p-4 mb-10">
+        <div className="grid grid-cols-3 gap-4 p-4 max-h-64 overflow-y-auto">
+          {cols.map((item) => (
+            <Input
+              key={item.key}
+              label={item.label}
+              value={values[item.key] || ""}
+              onChange={(e) => handleChange(item.key, e.target.value)}
+            />
+          ))}
         </div>
-      ))}
-    </div>
+      </div>
+
+      <Button type="submit">Submit</Button>
+    </form>
   );
 }
