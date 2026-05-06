4b59ddce17eb3f7a8be3aa52db4278ca29684279
Aziel Randel Rabano <imazielr@gmail.com>
2026-05-06 18:02:19 +0800

    update main temp

--- app/(protected)/users/page.tsx
+++ app/(protected)/users/page.tsx
@@ -1,6 +1,11 @@
 import DataTable from "@/component/ui/DataTable";
+import { getUsers } from "@/app/actions/userActions";
+import { mapToTableFormat } from "@/lib/mappers/projectMappers";
 
-export default function Users() {
+export default async function Users() {
+  const raw = await getUsers();
+  const { columns: newColumns, rows: newRows } = mapToTableFormat(raw);
+
   return (
     <div className="p-4 bg-[#140C2A]">
       <DataTable title={"users"} columns={} rows={} />
