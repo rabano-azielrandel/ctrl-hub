a50b589820e6c63d8349fd0397518b3c4a2aea9d
Aziel Randel Rabano <imazielr@gmail.com>
2026-05-07 11:38:35 +0800

    update performance component

---
+++ component/sections/analytics/performance.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Performance() {
+  return (
+    <div className="p-4 border border-[#B67DF2]">
+      <p>PERFORMANCE TARGETS</p>
+
+      <div className="flex">
+        {/* Slider */}
+        <div className="flex flex-col"></div>
+
+        {/* Half Donut */}
+      </div>
+    </div>
+  );
+}
