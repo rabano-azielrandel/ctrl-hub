6c40e153264d0defb24b8e7f02174af24108904b
Aziel Randel Rabano <imazielr@gmail.com>
2026-05-10 13:11:13 +0800

    update server action for expense tracker

--- app/actions/expense.ts
+++ app/actions/expense.ts
@@ -1,8 +1,14 @@
 "use server";
 
 import { createClient } from "@/lib/supabase/server";
+import { expenseColors } from "@/lib/data/expenseTracker";
+import { Category } from "@/types/ExpenseTracker";
 
-export async function getExpenseTypes() {
+export async function getExpenseTypes(): Promise<{
+  success: boolean;
+  data?: Category[];
+  error?: string;
+}> {
   const supabase = await createClient();
 
   const {
@@ -13,4 +19,27 @@ export async function getExpenseTypes() {
   if (userError || !user) {
     return { success: false, error: "Unauthorized: Not authenticated." };
   }
+
+  const { data: expenseType, error: expenseError } = await supabase
+    .from("expense_types")
+    .select("expense_type_id, name")
+    .eq("auth_id", user.id);
+
+  if (expenseError) {
+    return {
+      success: false,
+      error: expenseError.message || "Fetching Expenses Type Failed",
+    };
+  }
+
+  const categories: Category[] = expenseType.map((item, index) => ({
+    id: item.expense_type_id,
+    label: item.name,
+    color: expenseColors[index % expenseColors.length],
+  }));
+
+  return {
+    success: true,
+    data: categories,
+  };
 }
