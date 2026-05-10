a06f89c7457fc65052ee8c8d252ca5059f461d02
Aziel Randel Rabano <imazielr@gmail.com>
2026-05-10 10:45:25 +0800

    initial expense tracker and its data

---
+++ app/(protected)/expense/page.tsx
@@ -0,0 +1,8 @@
+export default function ExpenseTracker() {
+  return (
+    <div className="h-screen flex flex-col gap-4 p-4 bg-red-400">
+      <div className="h-[15%] bg-amber-400">a</div>
+      <div className="h-[85%] bg-blue-400">a</div>
+    </div>
+  );
+}
---
+++ lib/data/expenseTracker.ts
@@ -0,0 +1,18 @@
+export const summaryCards = [
+  {
+    title: "Monthly Salary",
+    amount: "₱ 1500",
+  },
+  {
+    title: "Total Spent",
+    amount: "₱ 1500",
+  },
+  {
+    title: "Savings",
+    amount: "₱ 1500",
+  },
+  {
+    title: "Total Expense Entries",
+    amount: "₱ 1500",
+  },
+];
